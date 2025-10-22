e96c3d118942eb0bd519204dde8f512819d8adc2
mohith182007 <mohith.25004765@jainuniversity.ac.in>
2025-10-22 07:33:53 +0530

    Update frontend API URL to use Render backend

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_BASE_URL = 'http://localhost:5000/api';
+const API_BASE_URL = 'https://survey-backend-ktjp.onrender.com/api';
 
 export const submitUserInfo = async (userData) => {
   try {
